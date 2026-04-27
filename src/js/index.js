8f146251db8554e09a07a97885c68cee6d4529de
ZAND1LE <zandilebluesky@gmail.com>
2026-04-27 16:20:08 +0900

    Update final styling project

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,22 @@
 import '../scss/style.scss'
+import Swiper from 'swiper'
 
-console.log('It works!')
+const burger = document.querySelector('.sidebar__burger')
+const sidebar = document.querySelector('.sidebar')
+
+if (burger && sidebar) {
+  burger.addEventListener('click', () => {
+    sidebar.classList.toggle('sidebar--open')
+  })
+}
+
+if (window.innerWidth < 768) {
+  new Swiper('.brands__slider', {
+    slidesPerView: 1.25,
+    spaceBetween: 16,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  })
+}
