6d351a26af47406c01a71d43a3bcac12008736ee
ZAND1LE <zandilebluesky@gmail.com>
2026-04-29 21:22:49 +0900

    modified font-weight and js

--- src/js/index.js
+++ src/js/index.js
@@ -2,19 +2,58 @@ import '../scss/style.scss'
 import Swiper from 'swiper'
 import { Pagination } from 'swiper/modules'
 
-const burger = document.querySelector('.sidebar__burger')
+const openBtn = document.querySelector('.main__menu-icon')
+
+const closeBtn = document.querySelector('.sidebar__burger')
+
 const sidebar = document.querySelector('.sidebar')
 
-if (burger && sidebar) {
-  burger.addEventListener('click', () => {
-    sidebar.classList.toggle('sidebar--open')
+if (openBtn && sidebar) {
+  openBtn.addEventListener('click', () => {
+    sidebar.classList.add('sidebar--open')
+  })
+}
+
+if (closeBtn && sidebar) {
+  closeBtn.addEventListener('click', () => {
+    sidebar.classList.remove('sidebar--open')
+  })
+}
+
+const brandsToggle = document.querySelector('.brands__toggle')
+const brandExtras = document.querySelectorAll('.brands__item--extra')
+
+if (brandsToggle && brandExtras.length) {
+  brandsToggle.addEventListener('click', () => {
+    brandExtras.forEach((item) => {
+      item.classList.toggle('brands__item--visible')
+    })
+
+    const text = brandsToggle.querySelector('span')
+
+    text.textContent = text.textContent === 'Show all' ? 'Hide' : 'Show all'
+  })
+}
+
+const equipmentToggle = document.querySelector('.equipment__toggle')
+const equipmentExtras = document.querySelectorAll('.equipment--extra')
+
+if (equipmentToggle && equipmentExtras.length) {
+  equipmentToggle.addEventListener('click', () => {
+    equipmentExtras.forEach((item) => {
+      item.classList.toggle('equipment--visible')
+    })
+
+    const text = equipmentToggle.querySelector('span')
+
+    text.textContent = text.textContent === 'Show all' ? 'Hide' : 'Show all'
   })
 }
 
 if (window.innerWidth < 768) {
   new Swiper('.brands__slider', {
     modules: [Pagination],
-    slidesPerView: 1.2,
+    slidesPerView: 1.25,
     spaceBetween: 16,
     pagination: {
       el: '.brands-pagination',
@@ -24,13 +63,14 @@ if (window.innerWidth < 768) {
 
   new Swiper('.equipment__slider', {
     modules: [Pagination],
-    slidesPerView: 1.2,
+    slidesPerView: 1.25,
     spaceBetween: 16,
     pagination: {
       el: '.equipment-pagination',
       clickable: true
     }
   })
+
   new Swiper('.service-price__slider', {
     modules: [Pagination],
     slidesPerView: 1.1,
